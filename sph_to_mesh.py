656b447bf521a810b94fa82f2f6c69c6dd48e585
Tim Lester <tfrancislester@gmail.com>
2022-11-19 13:38:08 -0500

    Added spherical to xyz conversion, saving. Results can be viewed in MeshLab.

---
+++ sph_to_mesh.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import math
+import numpy as np
+
+# Spherical coordinates:
+# rho is the distance from the origin (think radius)
+# theta is the angle the rho vector forms with the x axis; 0 <= theta < 2pi
+# phi is the angle the rho vector forms with the z axis; 0 <= phi < pi
+
+def sph_to_xyz(rho, phi, theta):
+    x = rho*math.sin(phi*(math.pi/180))*math.cos(theta*(math.pi/180))
+    y = rho*math.sin(phi*(math.pi/180))*math.sin(theta*(math.pi/180))
+    z = rho*math.cos(phi*(math.pi/180))
+
+    return x, y, z
+
+# Give a file name and spherical coords data
+def write_xyz(fn, data):
+
+    for i in range(data.shape[0]):
+        data[i, :] = sph_to_xyz(*data[i, :])
+
+    # xyz format expects space delimiting
+    np.savetxt(fn, data, delimiter=" ")
+
+if __name__ == "__main__":
+    data = np.loadtxt("test.csv", delimiter=",")
+
+    write_xyz("test.xyz", data)
